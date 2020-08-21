d0abe559a13304be585ab7744ed3226a1f50064a
Pavel Cikiliov <pavelchi91@gmail.com>
2020-08-21 11:59:26 +0300

    Assignment 1

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -13,6 +15,8 @@ app.use(morgan('dev'));
 //allows to pass body of request message that is formated in JSON
 app.use(bodyParser.json());
 app.use('/dishes', dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
 
 app.use((req, res, next) => {
     res.statusCode = 200;
